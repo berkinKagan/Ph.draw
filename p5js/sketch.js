3d57072088166043c8c514c1357f7bb1f04ed6d1
Berkin Kağan Ateş <kagan.ates@ug.bilkent.edu.tr>
2024-04-23 18:41:34 +0200

    Thickness

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,20 +1,22 @@
 // where the serial server is (your local machine):
-var host = '192.168.204.1:81';
+var host = '192.168.25.153:81';
 var socket; // the websocket
 var sensorValue = 0; // the sensor value
-
+ 
 var c = "black";
-var allColors = ["blue", "green", "red", "yellow", "black", "#F0F8FF","thickness"];
-
+var allColors = ["blue", "green", "red", "yellow", "black", "#F0F8FF"];
+ 
 var cIndex = 4;
-
+ 
 var xCor;
 var yCor;
 var prevXCor;
 var prevYCor;
-
+ 
 var indEll;
 var initial = true;
+var thicknessMode = false; // Track thickness mode
+ 
 var joyStickInitial = true;
 var blueBut = document.getElementById("blue");
 var greenBut = document.getElementById("green");
@@ -24,91 +26,88 @@ var blackBut = document.getElementById("black");
 var rubberBut = document.getElementById("rubber");
 var buttonList = [blueBut, greenBut, redBut, yellowBut, blackBut, rubberBut];
 var strokeVal = 3;
-let sketchLayer;
-
+let cursorLayer;
+ 
 var drawEnabled = true;
-
+ 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noCursor();
   noStroke();
-
-  sketchLayer = createGraphics(windowWidth, windowHeight);
-
+ 
+  cursorLayer = createGraphics(windowWidth, windowHeight);
+ 
   background("#F0F8FF");
-
+ 
   text("Thickness", 1000, 0,)
-
+ 
   slider = createSlider(1, 100, 20, 1);
   slider.position(1040, 100);
   slider.style('width', '180px');
-
+ 
   socket = new WebSocket('ws://' + host);
   socket.onopen = sendIntro;
   socket.onmessage = readMessage;
 }
-
+ 
 function drawCursor(x, y) {
   fill(255, 0, 0);
   noStroke();
   ellipse(x, y, 20, 20);
 }
-
+ 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
+ 
 function draw() {
   background("#F0F8FF")
-  image(sketchLayer, 0 , 0);
+  image(cursorLayer, 0 , 0);
   drawCursor(xCor, yCor);
-
+ 
   if (drawEnabled) {
-    sketchLayer.strokeWeight(slider.value());
-    sketchLayer.stroke(c); 
-    sketchLayer.line(xCor/2, yCor/2, prevXCor/2, prevYCor/2); 
+    cursorLayer.strokeWeight(slider.value());
+    cursorLayer.stroke(c); 
+    cursorLayer.line(xCor/2, yCor/2, prevXCor/2, prevYCor/2); 
   }  
 }
-
+ 
 function bluePress() {
   c = "blue";
   blueBut.style.backgroundColor = "blue";
 }
-
+ 
 function greenPress() {
   c = "green";
   greenBut.style.backgroundColor = "green";
 }
-
+ 
 function redPress() {
   c = "red";
   redBut.style.backgroundColor = "red";
 }
-
+ 
 function yellowPress() {
   c = "yellow";
   yellowBut.style.backgroundColor = "yellow";
 }
-
+ 
 function blackPress() {
   c = "black";
   blackBut.style.backgroundColor = "black";
 }
-
+ 
 function rubberPress() {
   c = "#F0F8FF";
   rubberBut.style.backgroundColor = "black";
 }
-
+ 
 function changeColor() {
   cIndex = (cIndex + 1) % allColors.length;
   c = allColors[cIndex];
   var btnn = buttonList[cIndex];
   btnn.style.backgroundColor = c;
   btnn.style.color = "white";
-
-  //if c == "thickness" -> drawEnabled = !drawEnabled
-  // thicknessMode = true
   
   for(var i = 0; i < buttonList.length; i++){
     if(i != cIndex){
@@ -117,51 +116,69 @@ function changeColor() {
     }
   }
 }
-
+ 
 function sendIntro() {
   // convert the message object to a string and send it:
   socket.send("Hello");
 }
-
+ 
 function readMessage(event) {
   var msg = event.data; // read data from the onmessage event
   sensorValue = msg;
-
+ 
   const coorArr = sensorValue.split(",");
-
-  //if thicknessMode == True
-
+ 
   if (coorArr[0] == 2) {
     drawEnabled = !drawEnabled;
+    thicknessMode = !thicknessMode; // Toggle thickness mode
   }
   else if (coorArr[0] == 3) {
     changeColor();
   }
   else {
-    if(joyStickInitial){
+    if (thicknessMode) {
+      handleThicknessMode(coorArr);
+    } else {
+      handleRegularMode(coorArr);
+    }
+  }
+}
+ 
+ 
+function handleThicknessMode(coorArr) {
+  if (Math.abs(coorArr[1] - 2000) > 200) {
+    // If there is significant joystick movement along the x-axis
+    var deltaX = mapJoystickInput(coorArr[1]); // Calculate the change in x-coordinate based on joystick input
+    xCor += deltaX; // Update the x-coordinate based on joystick input
+    
+    // Increment or decrement slider value based on joystick input
+    if (deltaX > 0) {
+      slider.value(slider.value() + 1); // Increment the slider value by one
+    } else if (deltaX < 0 && slider.value() > 1) {
+      slider.value(slider.value() - 1); // Decrement the slider value by one
+    }
+  }
+}
+ 
+function handleRegularMode(coorArr) {
+    if (joyStickInitial) {
       xCor = width/2;
       yCor = height/2;
       joyStickInitial = false;
     }
-  
     prevXCor = xCor;
     prevYCor = yCor;
-  
     if (Math.abs(coorArr[1] - 2000) > 200)
       xCor += mapJoystickInput(coorArr[1]);
-
+  
     if (Math.abs(coorArr[2] - 2000) > 200)
       yCor -= mapJoystickInput(coorArr[2]);
-  }
-
-  console.log(xCor, yCor);
-
-  //console.log(windowHeight, windowWidth);
-
-  println(sensorValue); // print it
   
+  //console.log(xCor, yCor);
+  //console.log(windowHeight, windowWidth);
+  //println(sensorValue); // print it
 }
-
+ 
 function mapJoystickInput(input) {
   return -1 + (2 * input) / 4095;
 }
